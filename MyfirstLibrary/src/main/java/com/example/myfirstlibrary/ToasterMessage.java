388a89a52535e480fd31cd0bc9ab5b23a92cc12b
Durgesh <djsawan>
2023-02-07 21:03:31 +0530

    Initial Commit

---
+++ MyfirstLibrary/src/main/java/com/example/myfirstlibrary/ToasterMessage.java
@@ -0,0 +1,12 @@
+package com.example.myfirstlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void makeText(Context c, String message){
+
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+
+    }
+}
